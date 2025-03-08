514e6ecb8547526fe4deb8c5272bafd0392b70fd
Krawstr <6balas132@gmail.com>
2025-03-08 04:53:49 -0300

    Projetos Proa

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const listaDeProjetos = [
+    "checkBox.html",
+    "glassmorphsm.html",
+    "neumorphismBtn.html",
+    "once.html",
+    "slidingEffect.html",
+    "loginRegistrationForm.html",
+    "amazonClone.html",
+    "siderbarMenu.html",
+    "websiteProject.html",
+    "portfolioWebProject.html",
+    "simpleSpinnerCharging.html",
+    "cartaoCredito.html",
+    "neuromorphismProfileCard.html",
+    "login.html",
+    "loadinPontos.html"
+];
+
+function carregarProjetos() {
+    let container = document.getElementById("projetos");
+
+    listaDeProjetos.forEach(projeto => {
+        let botao = document.createElement("a");
+        botao.href = `../pages/${projeto}`;
+        botao.innerText = projeto.replace(".html", ""); 
+        botao.classList.add("btn");
+
+        container.appendChild(botao);
+    });
+}
+
+carregarProjetos();
